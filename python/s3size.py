0fa6bb87a08c52266058dc04182227c4e0ffd4f5
chetanshell <37759377+chetanshell@users.noreply.github.com>
2020-02-06 13:12:42 +0530

    Python scripts

---
+++ python/s3size.py
@@ -0,0 +1,30 @@
+import boto3
+import os
+from datetime import datetime, timedelta
+
+os.environ['AWS_PROFILE'] = "prodaws"
+os.environ['AWS_DEFAULT_REGION'] = "eu-west-1"
+
+cw = boto3.client('cloudwatch',region_name='eu-west-1')
+s3client = boto3.client('s3',region_name='eu-west-1')
+
+allbuckets = s3client.list_buckets()
+
+for bucket in allbuckets['Buckets']:
+    print bucket['Name']
+    response = cw.get_metric_statistics(Namespace='AWS/S3',
+                                        MetricName='BucketSizeBytes',
+                                        Dimensions=[
+                                            {'Name': 'BucketName', 'Value': bucket['Name']},
+                                            {'Name': 'StorageType', 'Value': 'StandardStorage'}
+                                        ],
+                                        StartTime=datetime.now() - timedelta(days=1),
+                                        EndTime=datetime.now(),
+                                        Period=86400,
+                                        Statistics=['Average'],
+                                        Unit='Bytes'
+                                        )
+    # print(bucket)
+    bucket_size_bytes = response['Datapoints']
+    print(response)
+    break
