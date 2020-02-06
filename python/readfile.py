9b13cf02a5285514f9c41ccf8c764fad5ca21a6e
chetanshell <chetanramesh1@gmail.com>
2020-02-06 07:44:30 +0000

    adding file

--- python/readfile.py
+++ python/readfile.py
@@ -2,7 +2,7 @@ import os
 import boto3
 
 s3client = boto3.client('s3')
-bucket = open("/Users/chetan.r1/Desktop/buckets.txt", "r")
+bucket = open("/Users/cheta/Desktop/buckets.txt", "r")
 #def get_bucket_size(bucket):
 for i in bucket.readlines():
 	print(i.rstrip("\n"))
